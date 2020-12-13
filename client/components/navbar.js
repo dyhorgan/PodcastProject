7c78f63060e0d02960add6ba8f1ad604b9179c7c
Dylan Horgan <dyhorgan@gmail.com>
2020-12-13 17:20:28 -0500

    changing project to PodcastProject

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>Spring/Java Project!</h1>
+    <h1>Podcast Project!</h1>
     <nav>
       {isLoggedIn ? (
         <div>
